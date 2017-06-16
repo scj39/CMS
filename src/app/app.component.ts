d7b899aafad62d5d99168dfa2371d7cedb25cd5e
Smit Jain <scj39@cornell.edu>
2017-06-16 14:08:02 -0700

    added master/detail UI components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,13 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+	selector: 'my-app',
+	template: `
+	<div class="container">
+		<h1>{{title}}</h1>
+	</div>
+	`,
 })
-export class AppComponent  { name = 'Angular'; }
+export class AppComponent  { 
+	title = 'Student Management System'; 
+}
