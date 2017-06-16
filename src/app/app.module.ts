9c5685a641693be1aef86a632960808d392bc548
Smit Jain <scj39@cornell.edu>
2017-06-16 14:11:30 -0700

    declared new components in module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,8 @@ import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent }  from './app.component';
+import { Master } from './master.component'; 
+import { Detail } from './detail.component'; 
 
 @NgModule({
   imports:      [ BrowserModule ],
