28e3f81f6fc2a68d149d10f95c2c8cb71fc330a9
Smit Jain <scj39@cornell.edu>
2017-06-16 18:41:43 -0700

    finally got the mysql to play nice with express- the issue was not with express but rather the node.js mysql module

--- server/app.js
+++ server/app.js
@@ -5,8 +5,25 @@ app.get('/', function (req, res) {
 	res.send('Hello World!')
 })
 
-var server = app.listen(3660, function(){
+var server = app.listen(3600, function(){
 	var host = server.address().address; 
 	var port = server.address().port; 
-	console.log('listening at 3660', host, port); 
+	console.log('listening at 3600', host, port); 
 }); 
+
+var mysql = require('mysql'); 
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : 'password',
+  port: 4000
+});
+
+connection.connect(); 
+
+connection.query('SELECT 1 + 1 AS solution', function (err, rows, fields) {
+  if (err) throw err; 
+  console.log('The solution is: ', rows[0].solution); 
+}); 
+
+connection.end(); 
